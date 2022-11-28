488466723d436e7876ea16b49bf07cccd435432f
zhouzhou <zhouzhou@geostar.com.cn>
2022-11-28 20:31:48 +0800

    feat:分页组件样式修改，单位所属单位下拉接口对接

--- src/views/leftMenus/systemManagement/unitManagement/index.vue
+++ src/views/leftMenus/systemManagement/unitManagement/index.vue
@@ -213,6 +213,7 @@ export default {
               type: "success",
               message: "删除成功",
             });
+            this.params.pageNum=1;
             this.getUnitList();
           }
         });
