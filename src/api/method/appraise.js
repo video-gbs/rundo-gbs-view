ff3101db2324eea8dbf87d1717af3b79085917e3
wangbinlin <10742089@qq.com>
2022-12-08 17:28:43 +0800

    update 更新bug

--- src/api/method/appraise.js
+++ src/api/method/appraise.js
@@ -1,17 +1,17 @@
 // 评论管理
-import request from '@/api/fetch'
+import request from "@/api/fetch";
 
 export function addAppraise(params) {
   // 新增评价
-  return request.put(`/politics/backstage/appraise`, params)
+  return request.put(`/politics/backstage/appraise`, params);
 }
 
 export function editAffiche(params) {
   // 获取评价统计情况
-  return request.get(`/politics/backstage/appraise/stat`, params)
+  return request.get(`/politics/backstage/appraise/stat`, params);
 }
 
 export function getAppraiseByOne(id) {
   // 查看单条评价
-  return request.get(`/politics/backstage/appraise/${id}`)
+  return request.get(`/politics/appraise/${id}`);
 }
