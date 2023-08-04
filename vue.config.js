b36ac333b5381e3a0e5e6e050819d99fa92bbcf7
“zhouzhou” <zhouz@gzsendi.cn>
2023-08-04 11:32:06 +0800

    fix:优化

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ const defaultSettings = require('./src/settings.js')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CompressionWebpackPlugin = require('compression-webpack-plugin')
-const productionGzipExtensions = ['js', 'css']
+const productionGzipExtensions = ['js', 'css', 'less', 'sacc']
 
 const assetsCDN = {
   externals: {
@@ -113,7 +113,8 @@ module.exports = {
         algorithm: 'gzip',
         test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
         threshold: 10240,
-        minRatio: 0.8
+        minRatio: 0.3,
+        deleteOriginalAssets: false
       }),
       new webpack.optimize.LimitChunkCountPlugin({
         maxChunks: 5,
@@ -171,45 +172,45 @@ module.exports = {
       })
       .end()
 
-    config.when(process.env.NODE_ENV !== 'development', (config) => {
-      config
-        .plugin('ScriptExtHtmlWebpackPlugin')
-        .after('html')
-        .use('script-ext-html-webpack-plugin', [
-          {
-            inline: /runtime\..*\.js$/
-          }
-        ])
-        .end()
-      config.optimization.splitChunks({
-        chunks: 'all',
-        cacheGroups: {
-          libs: {
-            name: 'chunk-libs',
-            test: /[\\/]node_modules[\\/]/,
-            priority: 10,
-            chunks: 'initial'
-          },
-          elementUI: {
-            name: 'chunk-elementUI',
-            priority: 20,
-            test: /[\\/]node_modules[\\/]_?element-ui(.*)/
-          },
-          commons: {
-            name: 'chunk-commons',
-            test: resolve('src/components'),
-            minChunks: 3,
-            priority: 5,
-            reuseExistingChunk: true
-          }
+    // config.when(process.env.NODE_ENV !== 'development', (config) => {
+    config
+      .plugin('ScriptExtHtmlWebpackPlugin')
+      .after('html')
+      .use('script-ext-html-webpack-plugin', [
+        {
+          inline: /runtime\..*\.js$/
         }
-      })
-
-      config.optimization.runtimeChunk = {
-        name: (entrypoint) => `runtime~${entrypoint.name}`
+      ])
+      .end()
+    config.optimization.splitChunks({
+      chunks: 'all',
+      cacheGroups: {
+        libs: {
+          name: 'chunk-libs',
+          test: /[\\/]node_modules[\\/]/,
+          priority: 10,
+          chunks: 'initial'
+        },
+        elementUI: {
+          name: 'chunk-elementUI',
+          priority: 20,
+          test: /[\\/]node_modules[\\/]_?element-ui(.*)/
+        },
+        commons: {
+          name: 'chunk-commons',
+          test: resolve('src/components'),
+          minChunks: 3,
+          priority: 5,
+          reuseExistingChunk: true
+        }
       }
-
     })
+
+    config.optimization.runtimeChunk = {
+      name: (entrypoint) => `runtime~${entrypoint.name}`
+    }
+
+    // })
   },
   css: {
     loaderOptions: {
